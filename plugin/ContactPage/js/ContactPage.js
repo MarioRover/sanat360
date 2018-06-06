e8607c80769528cec2be7432e70a40fcf3a5bea9
MarioRover <hoa021@icloud.com>
2018-06-06 20:36:24 +0430

    Update Contact Site & Product details

--- plugin/ContactPage/js/ContactPage.js
+++ plugin/ContactPage/js/ContactPage.js
@@ -6,11 +6,14 @@ $(document).ready(function() {
 	});
 	$('#contact-page .contact-wrp .write-wrp form .input-wrp textarea').val('');
 });
-// var input = document.querySelectorAll('#contact-page .contact-wrp .write-wrp form input');
-// var textArea = document.querySelector('#contact-page .contact-wrp .write-wrp form .input-wrp textarea');
-// window.addEventListener('load' , function(){
-// 	input.forEach(function(item , index){
-// 		item.value = '';
-// 	})
-// 	textArea.value = '';
-// })
+
+// Company Map
+function initMap() {
+	// The location of Uluru
+	var uluru = { lat: -25.344, lng: 131.036 };
+	// The map, centered at Uluru
+	var map = new google.maps.Map(
+		document.getElementById('map'), { zoom: 4, center: uluru });
+	// The marker, positioned at Uluru
+	var marker = new google.maps.Marker({ position: uluru, map: map });
+}
