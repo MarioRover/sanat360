264de6337d1dcd596193717269e6ac6af38d9998
MarioRover <hoa021@icloud.com>
2018-06-11 15:39:37 +0430

    Create Blog Site
    create blog site and update news card js file

--- plugin/ArticleNewsCard/js/ArticleNewsCard.js
+++ plugin/ArticleNewsCard/js/ArticleNewsCard.js
@@ -1,5 +1,33 @@
-$(document).ready(function() {
-  $('.post-module').hover(function() {
-    $(this).find('.description').slideToggle(300);
-  })
+
+function showDescrib(post) {
+  $(post).addClass(`show`);
+  $(post).find('.description').slideDown(300);
+}
+function hiddenDescrib(post) {
+  $(post).removeClass(`show`);
+  $(post).find('.description').slideUp(300);
+}
+
+$(document).ready(function () {
+  let posts = document.querySelectorAll(`.post-module`);
+
+  $(posts).each(function (index, post) {
+    // Click Event
+    $(post).on(`click`, function () {
+      if (!$(this).hasClass(`show`)) {
+        // Show Description
+       showDescrib(post);
+      } else if ($(this).hasClass(`show`)) {
+        // Hidden Description
+        hiddenDescrib(post);
+      }
+    });
+    // Mouse Event
+    $(post).on(`mouseenter`, function () {
+      showDescrib(post);
+    });
+    $(post).on(`mouseleave`, function () {
+      hiddenDescrib(post);
+    });
+  });
 });
